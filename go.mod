7950f81994bff8ea6f7380516ffd945f03f10158
Richard Zak <richard.j.zak@gmail.com>
2022-02-25 13:56:36 -0500

    Replaced Go's webserver with `TinyWWW`, trying to fix #2

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/rjzak/web_wordpuzzle
 
 go 1.17
+
+require github.com/rjzak/tinywww v0.0.0-20220225184024-6723be5db9e7
+
+require (
+	github.com/alphahorizonio/tinynet v0.0.0-20210118222949-51439cf30be8 // indirect
+	github.com/alphahorizonio/unisockets v0.1.1 // indirect
+)
--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"github.com/rjzak/tinywww"
 	"math/rand"
-	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -25,38 +25,44 @@ func main() {
 	randIndex := rand.Intn(len(wordArray))
 	theWord := strings.ToLower(wordArray[randIndex])
 	fmt.Printf("Picked word %s from list of %d words.\n", theWord, len(wordArray))
-	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		_, err := w.Write(gameBoard)
+	wwwwServer, err := tinywww.NewTinyWWW("127.0.0.1:8080")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to listen on 127.0.0.1:8080: %s\n", err)
+		os.Exit(1)
+	}
+
+	wwwwServer.HandleGet("/", func(resp *tinywww.TinyResponse, req *tinywww.TinyRequest) {
+		resp.SetHeader("Content-Type", "text/html")
+		_, err := resp.Write(gameBoard)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing gameboard to http.ResponseWriter: %s\n", err)
+			fmt.Fprintf(os.Stderr, "Error writing gameboard client: %s\n", err)
 		}
 	})
 
-	mux.HandleFunc("/jquery.js", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/javascript")
-		_, err := w.Write(jquery)
+	wwwwServer.HandleGet("/jquery.js", func(resp *tinywww.TinyResponse, req *tinywww.TinyRequest) {
+		resp.SetHeader("Content-Type", "text/javascript")
+		_, err := resp.Write(jquery)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing jquery to http.ResponseWriter: %s\n", err)
+			fmt.Fprintf(os.Stderr, "Error writing jquery to client: %s\n", err)
 		}
 	})
 
-	mux.HandleFunc("/guess", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
-		var word string = ""
-		if err := req.ParseForm(); err == nil {
-			word = req.Form.Get("word")
+	wwwwServer.HandleGet("/guess", func(resp *tinywww.TinyResponse, req *tinywww.TinyRequest) {
+		resp.SetHeader("Content-Type", "text/plain")
+		word, okay := req.Form["word"]
+		result := "ccccc"
+		if okay {
+			result = process_guess(theWord, word)
 		}
-		_, err := w.Write([]byte(process_guess(theWord, word)))
+		_, err := resp.Write([]byte(result))
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error writing guess response to http.ResponseWriter: %s\n", err)
 		}
 	})
 
 	fmt.Fprintf(os.Stderr, "Listening on 127.0.0.1:8080...\n")
-	if err := http.ListenAndServe("127.0.0.1:8080", mux); err != nil {
+	if err := wwwwServer.ListenMaybeExit(false); err != nil {
 		fmt.Fprintf(os.Stderr, "HTTP Server error: %s\n", err)
 	}
 }
